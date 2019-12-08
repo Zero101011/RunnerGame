7b00553d3c8fea0980736453146af82c1e6c38dc
Zero101011 <geejay412@gmail.com>
2019-12-08 15:51:59 +0000

    Finalized build

--- RunnerGame/Assets/Scripts/HandleTextFile.cs
+++ RunnerGame/Assets/Scripts/HandleTextFile.cs
@@ -1,43 +1,43 @@
-﻿using UnityEngine;
-using UnityEditor;
-using System.IO;
+﻿//using UnityEngine;
+//using UnityEditor;
+//using System.IO;
 
-public class HandleTextFile : MonoBehaviour
-{
-    public PlayerController PlayerController;
+//public class HandleTextFile : MonoBehaviour
+//{
+//    public PlayerController PlayerController;
 
-    private void Start()
-    {
-        PlayerController = GetComponent<PlayerController>();
-    }
+//    private void Start()
+//    {
+//        PlayerController = GetComponent<PlayerController>();
+//    }
 
-    [MenuItem("Tools/Write file")]
-    static void WriteString()
-    {
-        string path = "Assets/Resources/test.txt";
+//    [MenuItem("Tools/Write file")]
+//    static void WriteString()
+//    {
+//        string path = "Assets/Resources/test.txt";
 
-        //Write some text to the test.txt file
-        StreamWriter writer = new StreamWriter(path, true);
-        writer.WriteLine("Death Position = " + PlayerController.Instance.deathPos);
-        writer.Close();
+//        //Write some text to the test.txt file
+//        StreamWriter writer = new StreamWriter(path, true);
+//        writer.WriteLine("Death Position = " + PlayerController.Instance.deathPos);
+//        writer.Close();
 
-        //Re-import the file to update the reference in the editor
-        AssetDatabase.ImportAsset(path);
-        TextAsset asset = Resources.Load<TextAsset>(path);
+//        //Re-import the file to update the reference in the editor
+//        AssetDatabase.ImportAsset(path);
+//        TextAsset asset = Resources.Load<TextAsset>(path);
 
-        //Print the text from the file
-        Debug.Log(asset.text);
-    }
+//        //Print the text from the file
+//        Debug.Log(asset.text);
+//    }
 
-    [MenuItem("Tools/Read file")]
-    static void ReadString()
-    {
-        string path = "Assets/Resources/test.txt";
+//    [MenuItem("Tools/Read file")]
+//    static void ReadString()
+//    {
+//        string path = "Assets/Resources/test.txt";
 
-        //Read the text from directly from the test.txt file
-        StreamReader reader = new StreamReader(path);
-        Debug.Log(reader.ReadToEnd());
-        reader.Close();
-    }
+//        //Read the text from directly from the test.txt file
+//        StreamReader reader = new StreamReader(path);
+//        Debug.Log(reader.ReadToEnd());
+//        reader.Close();
+//    }
 
-}
+//}
